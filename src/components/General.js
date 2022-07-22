33014545e1a55b17ea67c22849c28c91e5a2ae9d
Austin Keller <austinkllr@gmail.com>
2022-07-21 22:43:35 -0700

    form conditionally renders whether submit has been clicked or not

--- src/components/General.js
+++ src/components/General.js
@@ -1,20 +1,39 @@
 import React, { Component } from "react";
 
 class General extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      editMode: true,
+    };
+
+    this.onSubmitForm = this.onSubmitForm.bind(this);
+  }
+
+  onSubmitForm(e) {
+    e.preventDefault();
+    this.setState({
+      editMode: false,
+    });
+  }
+
   render() {
-    return (
-      <div>
-        <form>
-          <label htmlFor="name">Name: </label>
-          <input name="name"></input>
-          <label htmlFor="phone">Phone: </label>
-          <input name="phone"></input>
-          <label htmlFor="email">Email: </label>
-          <input name="email"></input>
-          <button>Submit</button>
-        </form>
-      </div>
-    );
+    if (this.state.editMode) {
+      return (
+        <div>
+          <form>
+            <label htmlFor="name">Name: </label>
+            <input name="name"></input>
+            <label htmlFor="phone">Phone: </label>
+            <input name="phone"></input>
+            <label htmlFor="email">Email: </label>
+            <input name="email"></input>
+            <button onClick={this.onSubmitForm}>Submit</button>
+          </form>
+        </div>
+      );
+    }
   }
 }
 
