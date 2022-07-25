e2a19ed2df2eb6ca91b87c814bb99efdb95f5350
Austin Keller <austinkllr@gmail.com>
2022-07-24 20:36:31 -0700

    once form submitted, renders divs with input values

--- src/components/General.js
+++ src/components/General.js
@@ -6,9 +6,13 @@ class General extends Component {
 
     this.state = {
       editMode: true,
+      name: "",
+      phone: "",
+      email: "",
     };
 
     this.onSubmitForm = this.onSubmitForm.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
   }
 
   onSubmitForm(e) {
@@ -18,19 +22,37 @@ class General extends Component {
     });
   }
 
+  onInputChange(e) {
+    if (e.target.name === "name") {
+      this.setState({ name: e.target.value });
+    } else if (e.target.name === "phone") {
+      this.setState({ phone: e.target.value });
+    } else {
+      this.setState({ email: e.target.value });
+    }
+  }
+
   render() {
     if (this.state.editMode) {
       return (
         <div>
           <form>
             <label htmlFor="name">Name: </label>
-            <input name="name"></input>
+            <input name="name" onChange={this.onInputChange}></input>
             <label htmlFor="phone">Phone: </label>
-            <input name="phone"></input>
+            <input name="phone" onChange={this.onInputChange}></input>
             <label htmlFor="email">Email: </label>
-            <input name="email"></input>
+            <input name="email" onChange={this.onInputChange}></input>
             <button onClick={this.onSubmitForm}>Submit</button>
           </form>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div>{this.state.name}</div>
+          <div>{this.state.phone}</div>
+          <div>{this.state.email}</div>
         </div>
       );
     }
